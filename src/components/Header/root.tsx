5fe1811c623fe2828c730c77f23ac054786314d0
Henrique Barbosa Sampaio <retr0lbb@gmail.com>
2024-05-03 21:01:08 -0300

    style: add more components to code

--- src/components/Header/root.tsx
+++ src/components/Header/root.tsx
@@ -1,7 +1,7 @@
 
 export const root: React.FC = () => {
     return(
-        <header className="w-screen flex min-h-24 sticky">
+        <header className="w-screen flex min-h-24 absolute bg-red-500 top-0 right-0 left-0">
 
         </header>
     )
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,48 +1,56 @@
-import Header from "@/components/Header/_index"
 import {CardBody, CardContainer, CardItem} from "@/components/ui/3d-card"
 import Image from "next/image"
+import {Meteors} from "@/components/ui/meteors"
+import { GridSmallBackgroundDemo } from "@/components/ui/gridBg";
 
 export default function Home() {
   return (
-    <main className="w-screen min-h-screen bg-black">
-      <Header.root>
-
-      </Header.root>
-
-      <section className="w-full grid grid-cols-2 p-20 place-items-center mt-20">
+    <main className="w-screen min-h-screen relative bg-black">
+      <section className="w-full h-screen grid grid-cols-2 p-20 place-items-center">
         {/**Text div main */}
         
         <div className="font-mono">
-          <h1 className="text-8xl text-white/10 selection:bg-gray-600/40">
+          <h1 className="text-8xl text-zinc-300 selection:bg-gray-600/40">
             Henrique Barbosa Full stack web developer
           </h1>
           <div className="flex items-center justify-start gap-4 mt-4">
-            <div>
-              <label className="text-gray-300/35" htmlFor="contact-btn">Want a website? </label>
-              <button id="contact-btn" className="px-2 py-1.5 bg-gray-50 rounded-md">Contact Me</button>
-            </div>
-            <div>
-              <label className="text-gray-300/35" htmlFor="contact-btn">Want to see more? </label>
-              <button id="contact-btn" className="px-2 py-1.5 bg-gray-50 rounded-md">My projects</button>
-            </div>
+            {/**
+             * 
+             * try diferent buttons settings
+             * 
+             *<div>
+                <label className="text-gray-300/35" htmlFor="contact-btn">Want a website? </label>
+                <button id="contact-btn" className="px-2 py-1.5 bg-gray-50 rounded-md">Contact Me</button>
+              </div>
+              <div>
+                <label className="text-gray-300/35" htmlFor="contact-btn">Want to see more? </label>
+                <button id="contact-btn" className="px-2 py-1.5 bg-gray-50 rounded-md">My projects</button>
+              </div>
+             */}
           </div>
         </div>
 
-        {/** photo div main */}
-        <div>
-          <CardContainer>
-            <CardBody>
-              <CardItem translateZ="100">
-                <Image 
-                  src="https://github.com/retr0lbb.png"
-                  alt="Avatar from retr0lbb github"
-                  width="1000"
-                  height="1000"
-                />
+        {/** photo div main 
+         * 
+         * 
+         * <div>
+          <CardContainer className="w-" containerClassName="">
+            <CardBody className="grid place-items-center bg-white/10 rounded">
+              <CardItem translateZ="50">
+                <div className="bg-gradient-to-br from-green-500 to-blue-300 size-64 rounded-lg" />
+              </CardItem>
+              <CardItem translateZ={40} className="w-full">
+                <div className="w-full flex items-center justify-evenly p-2">
+                  <button className="bg-white/20 size-12 rounded-full text-zinc-300 hover:bg-white/70 hover:text-zinc-700">G</button>
+                  <button className="bg-white/20 size-12 rounded-full text-zinc-300 hover:bg-white/70 hover:text-zinc-700">E</button>
+                  <button className="bg-white/20 size-12 rounded-full text-zinc-300 hover:bg-white/70 hover:text-zinc-700">L</button>
+                </div>
               </CardItem>
             </CardBody>
           </CardContainer>
         </div>
+        */}
+        <GridSmallBackgroundDemo />
       </section>
     </main>
   );
