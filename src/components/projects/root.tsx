34e25df2086d2f7f62f76596b4cc76ac71bd6857
Henrique Barbosa Sampaio <retr0lbb@gmail.com>
2024-05-05 01:18:37 -0300

    style: set new padding to root

--- src/components/projects/root.tsx
+++ src/components/projects/root.tsx
@@ -1,6 +1,6 @@
 import React, {ComponentProps} from "react"
 import { twMerge } from "tailwind-merge"
-import {Github} from "lucide-react"
+import {Github, Linkedin, Globe} from "lucide-react"
 import { IconButton } from "../iconButton"
 
 
@@ -12,18 +12,30 @@ interface rootProps extends ComponentProps<"div">{
 export const root: React.FC<rootProps> = ({...props}) => {
     return(
         <div className={
-            twMerge("w-full h-full bg-gradient-to-br group relative hover: from-white/20 to-white/30 rounded-2xl p-5 transition-all overflow-hidden",
+            twMerge("w-full h-full bg-gradient-to-br group relative p-4 hover: from-white/20 to-white/30 rounded-2xl transition-all overflow-hidden",
             props.className)
         }>
             {props.children}
 
             <div
-                className="absolute bg-black/70 inset-0 group-hover:translate-y-0 transition-all 
-                duration-300 translate-y-full grid place-items-center p-20"
+                className="absolute bg-black/70 inset-0 group-hover:translate-x-0 transition-all 
+                duration-75 -translate-x-full p-4 grid"
             >
-                <div>
+                <div className="">
+                    <h1 className="text-4xl text-white/90 font-bold">N.E.R.D</h1>
+                    <p className="text-white/70">A digital game store, made using React, Nodejs and MongodbAtlas</p>
+                </div>
+                <div className="flex flex-row-reverse items-end gap-4 p-2">
                     <IconButton >
                         <Github />
+                    </IconButton>
+
+                    <IconButton >
+                        <Linkedin />
+                    </IconButton>
+
+                    <IconButton >
+                        <Globe />
                     </IconButton>
                 </div>
             </div>
