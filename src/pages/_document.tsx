57e03aafa57ab5412baf9ad980e01b838b9f88fa
Henrique Barbosa Sampaio <retr0lbb@gmail.com>
2024-05-06 22:18:17 -0300

    style: add margin top to grid, refactor grid rows

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -4,7 +4,7 @@ export default function Document() {
   return (
     <Html className="p-0 m-0 box-border" lang="en">
       <Head></Head>
-      <body className="overflow-y-hidden overflow-x-auto">
+      <body>
         <Main />
         <NextScript />
       </body>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,25 +1,40 @@
 import ProjectCard from "@/components/projects/index"
+import voteNow from "@/static/Home-black.png"
+import Motion from "@/static/70574400-9e6a-11e9-8708-22d4bf4c3322.png"
+import avatar from "@/static/Open Peeps - Bust.png"
 
 export default function Home() {
   return (
-    <div className="w-screen min-h-screen flex flex-col items-center justify-center bg-black overflow-hidden">
-      <section className="w-full grid grid-cols-5 gap-4 bg-red-700 p-4">
+    <div className="max-w-screen min-h-screen flex flex-col items-center justify-center bg-black">
+      
+      <section className="w-full grid grid-cols-5 gap-4 p-4 mt-20">
 
-          <section className="text-9xl text-white p-5 row-span-3 font-bold font-mono bg-green-50 col-span-3 overflow-hidden">
+          <section className="text-9xl text-white p-5 row-span-3 font-bold font-mono col-span-3 overflow-hidden">
             <p>Henrique Barbosa</p>
             <p>Full stack developer</p>
           </section>
 
           <div className="col-span-2 row-span-3 bg-blue-500">
-
           </div>
 
           <div className="col-span-5 bg-black px-4 py-6 text-5xl text-white font-mono font-bold">
             Projetos:
           </div>
 
-        <ProjectCard.root className="col-span-2">
+        <ProjectCard.root className="col-span-2" title="N.E.R.D" description="Uma loja online de jogos digitais focada na distribuição de jogos nacionais">
           <ProjectCard.image src="https://github.com/retr0lbb.png" />
+        </ProjectCard.root>
+
+        <ProjectCard.root className="col-span-3 row-span-2" 
+          title="Vote Now" 
+          description="Uma aplicação de votação em enquetes simples e facil com integração com web sockets e redis database"
+        >
+
+          <ProjectCard.image src={voteNow.src} />
+        </ProjectCard.root>
+
+        <ProjectCard.root className="col-span-2" title="Framer Motion" description="lorem ipos odsiad  aodsio ewijk jhiuoisdak idjsaokldakd ">
+          <ProjectCard.image src={Motion.src} />
         </ProjectCard.root>
 
 
