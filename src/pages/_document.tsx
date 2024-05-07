97a163f2172fa763065c26a9dfb76f0834bb149e
Henrique Barbosa Sampaio <retr0lbb@gmail.com>
2024-05-06 21:36:48 -0300

    style: using bento grid for all layout

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -4,7 +4,7 @@ export default function Document() {
   return (
     <Html className="p-0 m-0 box-border" lang="en">
       <Head></Head>
-      <body>
+      <body className="overflow-y-hidden overflow-x-auto">
         <Main />
         <NextScript />
       </body>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,29 +3,25 @@ import ProjectCard from "@/components/projects/index"
 export default function Home() {
   return (
     <div className="w-screen min-h-screen flex flex-col items-center justify-center bg-black overflow-hidden">
+      <section className="w-full grid grid-cols-5 gap-4 bg-red-700 p-4">
 
-      <section className="w-full p-5 grid grid-cols-5 gap-4 place-items-center">
-        <div className="col-span-5">
-          <section className="text-9xl text-white p-5 font-bold font-mono w-full h-full">
+          <section className="text-9xl text-white p-5 row-span-3 font-bold font-mono bg-green-50 col-span-3 overflow-hidden">
             <p>Henrique Barbosa</p>
             <p>Full stack developer</p>
           </section>
-        </div>
+
+          <div className="col-span-2 row-span-3 bg-blue-500">
+
+          </div>
+
+          <div className="col-span-5 bg-black px-4 py-6 text-5xl text-white font-mono font-bold">
+            Projetos:
+          </div>
+
         <ProjectCard.root className="col-span-2">
           <ProjectCard.image src="https://github.com/retr0lbb.png" />
         </ProjectCard.root>
 
-        <ProjectCard.root className="col-span-3 row-span-2">
-          <div className="size-40 bg-red-50"></div>
-        </ProjectCard.root>
-
-        <ProjectCard.root>
-          <div className="size-40 bg-red-50"></div>
-        </ProjectCard.root>
-
-        <ProjectCard.root>
-          <div className="size-40 bg-red-50"></div>
-        </ProjectCard.root>
 
       </section>
     </div>
