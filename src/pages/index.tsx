7d7999699c6061a1a2963b502a739c50c3cbecbe
Henrique Barbosa Sampaio <retr0lbb@gmail.com>
2024-05-08 21:26:08 -0300

    style: add glassmorphism

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,11 +4,23 @@ import Motion from "@/static/70574400-9e6a-11e9-8708-22d4bf4c3322.png"
 import HeroSection from "@/components/heroCard"
 import HeroHeader from "@/components/Header/_index"
 
+import { LucideHome, Folder, User2} from "lucide-react"
+
 export default function Home() {
   return (
-    <div className="max-w-screen min-h-screen flex flex-col items-center overflow-y-hidden justify-center bg-black relative">
-      <HeroHeader.root>
+    <div id="home" className="max-w-screen min-h-screen flex flex-col items-center overflow-y-hidden justify-center bg-black relative">
+      <HeroHeader.root className="justify-between">
+        <nav className="text-white">
+          <ul className="flex items-center justify-evenly gap-4 text-xl">
+            <li className="flex items-center justify-center gap-1"><LucideHome /><a href="#home">Home</a></li>
+            <li className="flex items-center justify-center gap-1"><Folder /><a href="#home">Projetos</a></li>
+            <li className="flex items-center justify-center gap-1"><User2 /><a href="#home">Sobre mim</a></li>
+          </ul>
+        </nav>
 
+        <div>
+          <input type="checkbox" />
+        </div>
       </HeroHeader.root>
       <section className="w-full grid grid-cols-5 gap-4 p-4 mt-24">
 
