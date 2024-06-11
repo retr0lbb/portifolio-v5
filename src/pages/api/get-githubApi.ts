dcad6cd2cebf425fa11cf78bdc8b9edc032604ca
Henrique Barbosa Sampaio <retr0lbb@gmail.com>
2024-06-11 12:55:39 -0300

    feat: consuming github api

--- src/pages/api/get-githubApi.ts
+++ src/pages/api/get-githubApi.ts
@@ -1,6 +1,58 @@
 import type { NextApiRequest, NextApiResponse } from "next";
-
+import axios from "axios"
+import { isNullOrUndefined } from "util";
 
 export default async function handler(request: NextApiRequest, reply: NextApiResponse){
-    return reply.send({message: "Hey man"})
+    const url = "https://api.github.com/users/retr0lbb/repos"
+    const token = process.env.GITHUB_API_KEY
+
+
+    interface repoMainProps {
+        name: string,
+    }
+
+    async function getRepos(url:string): Promise<repoMainProps[]>{
+        const response = await axios.get(url, {
+            headers: {
+                'Authorization': `token ${token}`,
+                'Accept': 'application/vnd.github.v3+json'
+            }
+        })
+        return response.data
+    }
+
+    async function getLatestCommit(repo: string): Promise<any>{
+        const url = `/repos/retr0lbb/${repo}/commits`;
+        const response = await axios.get(url, {
+            headers: {
+                'Authorization': `token ${token}`,
+                'Accept': 'application/vnd.github.v3+json'
+            }
+        });
+
+        const commits = response.data
+        return commits.length > 0 ? commits[0]: null;
+    }
+
+    try {
+        const repos = await getRepos(url)
+
+        if(repos.length === 0){
+            return reply.status(404).json({ message: 'Nenhum repositório encontrado.' });
+        }
+
+        let mostRecentCommit: any = null
+        let mostRecentRepo: string | null = null
+
+        for(const repo in repos){
+            console.log()
+        }
+        
+    } catch (error) {
+        
+    }
+    
+
+
+    //return reply.send({data})
 }
