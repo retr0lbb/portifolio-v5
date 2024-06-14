07977f3b89dbd2124d4c5642b0d64ae13a00ea08
Henrique Barbosa Sampaio <retr0lbb@gmail.com>
2024-06-14 12:28:48 -0300

    feat: starting to build fonts

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,6 +3,7 @@ import "@/styles/glassy.css"
 import type { AppProps } from "next/app";
 import {Providers as ThemeProvider} from "@/providers/theme.provider"
 import { Providers } from "@/providers/nextUi.provider"
+import {} from "@next/font/google"
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
